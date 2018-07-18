131daad34ccbe134371a39677f2362c5952b8a42
Matthew Riach <matthewriach@Matthews-MacBook-Air.local>
2018-07-18 12:12:09 -0600

    all fuctions are working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-let rng = 0
 const answer = document.querySelector('#answer')
 const answerList = document.querySelector('.answerList')
 const h1El = document.getElementById('randomQuote')
+const button = document.querySelector('button')
+const scoreText = document.querySelector('#scoreText')
+let rng = 0
+let numberOfQuotesGuessed = 0 
+let wrongGuesses = 0
+let correctGuesses = 0 
+let shouldResetGame = false
+
+//getting quotes from the Breaking Bad and Ron Swanson API
 
 function getBreakingBadQuote() {
     const url = "https://breaking-bad-quotes.herokuapp.com/v1/quotes"
@@ -19,6 +27,8 @@ function getRonSwansonQuote() {
         .then(console.log('Ron'))
 }
 
+//randomly choosing which API to pull from and adding it to the page
+
 function randomNumberGenerator() {
     return Math.floor((Math.random() * 10) + 1)
 }
@@ -31,25 +41,30 @@ function selectAPI() {
 selectAPI()
 
 function appendQuoteRon(quote) {
-    h1El.innerText = quote
+    h1El.innerText = `"${quote}"`
 }
 function appendQuoteBreaking(quote) {
-    h1El.innerText = quote[0].quote
+    h1El.innerText = `"${quote[0].quote}"`
 }
 
+//getting a new quote from either API when clicking the new quote button
+
 function changeQuoteButton() {
-    const button = document.querySelector('button')
     button.addEventListener('click', event => {
+        numberOfQuotesGuessed = 0
         selectAPI()
+        shouldResetGame === true ? resetGame() : null 
     })
 }
 
 changeQuoteButton()
 
+//correct or wrong click listeners on the images and adding answers to list below
+
 function breakingBadGuessListener() {
     const guessButton = document.querySelector('#breakingGuess')
     guessButton.addEventListener('click', event => {
-        rng % 2 == 0 ? correctAnswer(): answer.innerHTML = "wrong"
+        rng % 2 == 0 ? correctAnswer(): wrongAnswer()
     })
 }
 
@@ -58,21 +73,59 @@ breakingBadGuessListener()
 function ronGuessListener() {
     const guessButton = document.querySelector('#ronGuess')
     guessButton.addEventListener('click', event => {
-        rng % 2 == 1 ? correctAnswer() : answer.innerHTML = "wrong"
+        rng % 2 == 1 ? correctAnswer() : wrongAnswer()
     })
 }
 
+ronGuessListener()
+
 function correctAnswer() {
     answer.innerText = "correct"
-    createAnswerListEl(`${h1El.innerText} Correct`)
+    if(numberOfQuotesGuessed === 0) {
+        correctGuesses += 1
+        createAnswerListEl(`Correct - ${h1El.innerText}`)
+
+    }
+}
+
+function wrongAnswer() {
+    answer.innerText = "wrong"
+    if(numberOfQuotesGuessed === 0) {
+        wrongGuesses += 1 
+        createAnswerListEl(`Wrong - ${h1El.innerText}`)
+    }
 }
 
 function createAnswerListEl(quote) {
     let listEl = document.createElement('li')
+    listEl.setAttribute('class', 'answers')
     listEl.innerText = quote 
     answerList.appendChild(listEl)
+    numberOfQuotesGuessed += 1
+    endgame()
 }
 
-ronGuessListener()
+//resets the game after 5 guesses
+
+function endgame() {
+    answerList.childElementCount === 5 ? score() : null
+}
+
+function score() {
+    shouldResetGame = !shouldResetGame
+    scoreText.innerText = `You got ${correctGuesses} right and ${wrongGuesses} wrong!`
+    button.innerText = 'Play Again'
+}
+
+function resetGame() {
+    shouldResetGame = !shouldResetGame
+    while (answerList.firstChild) {
+        answerList.removeChild(answerList.firstChild)
+    }
+    scoreText.innerText = ''
+    button.innerText = 'New Quote'
+    answer.innerText = ''
+
+}
 
 
